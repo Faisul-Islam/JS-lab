f37cc18abb6f3fe8820cab07323baa4543d15ecc
Faisul Islam <suhid15@gmail.com>
2019-12-17 15:21:20 +0600

    anonymous callback

--- callback.js
+++ callback.js
@@ -1,1 +1,12 @@
 /* understanding how callbacks works*/
+function cbFunc(msg,callback){
+        console.log("Without callback " + msg);
+        
+        if(typeof callback == "function"){
+          callback();
+        }
+        
+      }
+      cbFunc("hello", function callbackFunction(){
+          console.log("This is callback func ");
+        });
