c502021da13d1306e3e187c4064e20e220eb7c70
Faisul-Islam <suhid15@gmail.com>
2022-02-23 23:59:43 +0600

    promise task 2

--- arrayMehtods.js
+++ arrayMehtods.js
@@ -1,4 +1,4 @@
-// my solution of https://javascript.info/task/camelcase
+// my solution of camelcase
 
 function camelize(input) {
     return input.split("-").map(function(itm, idx) {
@@ -7,7 +7,21 @@ function camelize(input) {
     }).join("");
 }
 
-// my solution of https://javascript.info/task/filter-range
+// my solution of filter-range
 function filterRange(arr, a, b) {
     return filteredArr = arr.filter(itm => (itm >= a && itm <= b))
+}
+// solution of map-objects
+
+let usersMapped = users.map(user => {
+    let ret = {};
+    ret.fullName = user.name + " " + user.surname;
+    ret.id = user.id;
+    return ret;
+})
+
+// solution to average-age
+
+function getAverageAge(arr) {
+    return arr.reduce((s, c) => s + c.age, 0) / arr.length;
 }
